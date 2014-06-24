a5870479ebd2ecdfda529753f42d3d64a9291ed5
Luan fonseca de farias <luanfonceca@gmail.com>
2014-06-24 05:13:34 -0300

    Fix the test_wsgi.

--- deck/tests/test_wsgi.py
+++ deck/tests/test_wsgi.py
@@ -8,7 +8,8 @@ from speakerfight.wsgi import application
 
 class WSGITest(TestCase):
     def test_assert_django_settings_module(self):
-        self.assertEquals('settings', environ.get('DJANGO_SETTINGS_MODULE'))
+        self.assertEquals('speakerfight.settings',
+                          environ.get('DJANGO_SETTINGS_MODULE'))
 
     def test_assert_application(self):
         self.assertIsInstance(application, WSGIHandler)
