bb1f18fc27a3c9f46a8fba741aeb5c3f29a517fe
Luan fonseca de farias <luanfonceca@gmail.com>
2014-06-24 05:36:27 -0300

    Add the loaddata django command in fabric.

--- fabfile.py
+++ fabfile.py
@@ -56,3 +56,16 @@ def deploy():
     _update_app()
     _restart_app()
     print green('Deploy succefully done!')
+
+
+def load_initial_data():
+    fixtures = [
+        'deck/fixtures/user.json',
+        'deck/fixtures/event.json',
+        'deck/fixtures/proposal.json',
+    ]
+    with cd(env.app_dir):
+        print yellow('Collecting the initial data')
+        for fixture in fixtures:
+            _run('manage.py loaddata {}'.format(fixture))
+        print green('Data succefully loaded')
