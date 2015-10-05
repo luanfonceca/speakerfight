9712c92b0d343ad7490c6e494b345772e057aaf4
Luan fonseca de farias <luanfonceca@gmail.com>
2015-10-05 16:38:33 -0300

    #107 - Adding total of events, proposals and users to the site.

--- core/urls.py
+++ core/urls.py
@@ -1,10 +1,11 @@
 from django.conf.urls import patterns, url
-from django.views.generic import TemplateView
 from django.views.i18n import javascript_catalog
+
+from core.views import IndexView
 
 urlpatterns = patterns(
     '',
-    url(r'^$', TemplateView.as_view(template_name="index.html"),
+    url(r'^$', IndexView.as_view(),
         name='index_page'),
     url(r'^jsi18n/$', javascript_catalog, name='javascript_catalog'),
 )
