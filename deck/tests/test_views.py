0360489f95bc6474c6c4c011fd266d8a73fcfa71
Bernardo Fontes <bernardoxhc@gmail.com>
2017-10-02 19:58:44 -0300

    Passo 2: discussão sobre utilidade do setUp e como ele pode mascarar a complexidade do teste/código

--- deck/tests/test_views.py
+++ deck/tests/test_views.py
@@ -11,11 +11,11 @@ from deck.models import Vote
 
 class CreateEventScheduleViewTests(TestCase):
 
-    def test_renders_correct_template_if_user_belongs_to_jury(self):
+    def setUp(self):
         # DB Setup
         tomorrow = timezone.now() + timedelta(days=1)
-        event = mommy.make('deck.Event', _fill_optional=['jury'], slots=3, due_date=tomorrow)
-        proposals = mommy.make('deck.Proposal', event=event, is_approved=False, _quantity=5)
+        self.event = mommy.make('deck.Event', _fill_optional=['jury'], slots=3, due_date=tomorrow)
+        proposals = mommy.make('deck.Proposal', event=self.event, is_approved=False, _quantity=5)
         approved_proposals = [
             mommy.make(Vote, proposal=proposals[0], rate=Vote.LAUGHING),
             mommy.make(Vote, proposal=proposals[1], rate=Vote.HAPPY),
@@ -26,56 +26,38 @@ class CreateEventScheduleViewTests(TestCase):
             mommy.make(Vote, proposal=proposals[4], rate=Vote.ANGRY),
         ]
 
+        self.url = reverse('create_event_schedule', args=[self.event.slug])
+
+    def test_renders_correct_template_if_user_belongs_to_jury(self):
         # Request Setup
         user = mommy.make(settings.AUTH_USER_MODEL)
-        event.jury.users.add(user)
+        self.event.jury.users.add(user)
         self.client.force_login(user)
-        url = reverse('create_event_schedule', args=[event.slug])
 
         # HTTP Request
-        response = self.client.get(url)
+        response = self.client.get(self.url)
 
         self.assertEqual(200, response.status_code)
         self.assertTemplateUsed(response, 'event/event_create_schedule.html')
 
     def test_redirect_to_view_event_if_user_does_not_belong_to_jury(self):
-        # DB Setup
-        tomorrow = timezone.now() + timedelta(days=1)
-        event = mommy.make('deck.Event', _fill_optional=['jury'], slots=3, due_date=tomorrow)
-
         # Request Setup
         user = mommy.make(settings.AUTH_USER_MODEL)
         self.client.force_login(user)
-        url = reverse('create_event_schedule', args=[event.slug])
 
         # HTTP Request
-        response = self.client.get(url)
-        redirect_url = reverse('view_event', args=[event.slug])
+        response = self.client.get(self.url)
+        redirect_url = reverse('view_event', args=[self.event.slug])
 
         self.assertRedirects(response, redirect_url)
 
     def test_renders_correct_template_if_superuser(self):
-        # DB Setup
-        tomorrow = timezone.now() + timedelta(days=1)
-        event = mommy.make('deck.Event', _fill_optional=['jury'], slots=3, due_date=tomorrow)
-        proposals = mommy.make('deck.Proposal', event=event, is_approved=False, _quantity=5)
-        approved_proposals = [
-            mommy.make(Vote, proposal=proposals[0], rate=Vote.LAUGHING),
-            mommy.make(Vote, proposal=proposals[1], rate=Vote.HAPPY),
-            mommy.make(Vote, proposal=proposals[2], rate=Vote.SAD),
-        ]
-        not_approved_proposals = [
-            mommy.make(Vote, proposal=proposals[3], rate=Vote.SLEEPY),
-            mommy.make(Vote, proposal=proposals[4], rate=Vote.ANGRY),
-        ]
-
         # Request Setup
         user = mommy.make(settings.AUTH_USER_MODEL, is_superuser=True)
         self.client.force_login(user)
-        url = reverse('create_event_schedule', args=[event.slug])
 
         # HTTP Request
-        response = self.client.get(url)
+        response = self.client.get(self.url)
 
         self.assertEqual(200, response.status_code)
         self.assertTemplateUsed(response, 'event/event_create_schedule.html')
