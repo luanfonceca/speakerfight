b1673fa12bfa3989774d586296a3c0dbabee55b7
Bernardo Fontes <bernardoxhc@gmail.com>
2017-10-02 19:56:14 -0300

    Passo 1: Mostrar complexidade na escrita de testes adicionais que tem muito pouco a ver com o banco ou a funcionalidade em si

--- deck/tests/test_views.py
+++ deck/tests/test_views.py
@@ -37,3 +37,45 @@ class CreateEventScheduleViewTests(TestCase):
 
         self.assertEqual(200, response.status_code)
         self.assertTemplateUsed(response, 'event/event_create_schedule.html')
+
+    def test_redirect_to_view_event_if_user_does_not_belong_to_jury(self):
+        # DB Setup
+        tomorrow = timezone.now() + timedelta(days=1)
+        event = mommy.make('deck.Event', _fill_optional=['jury'], slots=3, due_date=tomorrow)
+
+        # Request Setup
+        user = mommy.make(settings.AUTH_USER_MODEL)
+        self.client.force_login(user)
+        url = reverse('create_event_schedule', args=[event.slug])
+
+        # HTTP Request
+        response = self.client.get(url)
+        redirect_url = reverse('view_event', args=[event.slug])
+
+        self.assertRedirects(response, redirect_url)
+
+    def test_renders_correct_template_if_superuser(self):
+        # DB Setup
+        tomorrow = timezone.now() + timedelta(days=1)
+        event = mommy.make('deck.Event', _fill_optional=['jury'], slots=3, due_date=tomorrow)
+        proposals = mommy.make('deck.Proposal', event=event, is_approved=False, _quantity=5)
+        approved_proposals = [
+            mommy.make(Vote, proposal=proposals[0], rate=Vote.LAUGHING),
+            mommy.make(Vote, proposal=proposals[1], rate=Vote.HAPPY),
+            mommy.make(Vote, proposal=proposals[2], rate=Vote.SAD),
+        ]
+        not_approved_proposals = [
+            mommy.make(Vote, proposal=proposals[3], rate=Vote.SLEEPY),
+            mommy.make(Vote, proposal=proposals[4], rate=Vote.ANGRY),
+        ]
+
+        # Request Setup
+        user = mommy.make(settings.AUTH_USER_MODEL, is_superuser=True)
+        self.client.force_login(user)
+        url = reverse('create_event_schedule', args=[event.slug])
+
+        # HTTP Request
+        response = self.client.get(url)
+
+        self.assertEqual(200, response.status_code)
+        self.assertTemplateUsed(response, 'event/event_create_schedule.html')
