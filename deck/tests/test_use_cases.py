20c3bf7078cd8fc6002daa46b16f8a22ee495f17
Bernardo Fontes <bernardoxhc@gmail.com>
2017-10-03 18:46:41 -0300

    Passo 08: refatoração para usar o set up como apoio para os testes

--- deck/tests/test_use_cases.py
+++ deck/tests/test_use_cases.py
@@ -8,21 +8,34 @@ from deck.use_cases import initialize_event_schedule
 
 class InitializeEventScheduleTestCase(TestCase):
 
-    def test_initialize_event_schedule_with_top_approved_proposals(self):
-        not_scheduled_proposals = [Mock(Proposal), Mock(Proposal)]
-        track = Mock(Track)
-        track.has_activities.return_value = False
-        event = Mock(Event)
-        event.get_main_track.return_value = track
-        event.filter_not_scheduled_by_slots.return_value = not_scheduled_proposals
+    def setUp(self):
+        self.track = Mock(Track)
+        self.event = Mock(Event)
+        self.event.get_main_track.return_value = self.track
 
-        executed = initialize_event_schedule(event)
+    def test_initialize_event_schedule_with_top_approved_proposals(self):
+        self.track.has_activities.return_value = False
+        not_scheduled_proposals = [Mock(Proposal), Mock(Proposal)]
+        self.event.filter_not_scheduled_by_slots.return_value = not_scheduled_proposals
+
+        executed = initialize_event_schedule(self.event)
         self.assertTrue(executed)
 
-        event.get_main_track.assert_called_once_with()
-        track.has_activities.assert_called_once_with()
-        event.filter_not_scheduled_by_slots.assert_called_once_with()
-        track.add_proposal_to_slot.assert_has_calls([
+        self.event.get_main_track.assert_called_once_with()
+        self.track.has_activities.assert_called_once_with()
+        self.event.filter_not_scheduled_by_slots.assert_called_once_with()
+        self.track.add_proposal_to_slot.assert_has_calls([
             call(not_scheduled_proposals[0], 0),
             call(not_scheduled_proposals[1], 1),
         ])
+
+    def test_do_not_initialize_event_schedule_if_main_track_already_has_activities(self):
+        self.track.has_activities.return_value = True
+
+        executed = initialize_event_schedule(self.event)
+        self.assertTrue(executed)
+
+        self.event.get_main_track.assert_called_once_with()
+        self.track.has_activities.assert_called_once_with()
+        self.assertFalse(self.event.filter_not_scheduled_by_slots.called)
+        self.assertFalse(self.track.add_proposal_to_slot.called)
