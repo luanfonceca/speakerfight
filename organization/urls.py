6e50c84076ceda34a7cae5fce347f76dbaee8a48
Luan Fonseca <luanfonceca@gmail.com>
2017-10-04 16:58:00 -0300

    #130 - Fix url includes.

--- organization/urls.py
+++ organization/urls.py
@@ -4,10 +4,10 @@ from smarturls import surl as url
 from . import views
 
 urlpatterns = (
-    url(regex=r'/organizations/create/',
+    url(regex=r'/create/',
         view=views.CreateOrganization.as_view(),
         name='create_organization'),
-    url(regex=r'/organizations/<slug:slug>/update/',
+    url(regex=r'/<slug:slug>/update/',
         view=views.UpdateOrganization.as_view(),
         name='update_organization'),
 )
