b735ce6c4e5450eb2c8c6a6b0f33cfb630675488
Bernardo Fontes <bernardoxhc@gmail.com>
2017-10-05 09:31:16 -0300

    Use single return statement

--- deck/permissions.py
+++ deck/permissions.py
@@ -1,5 +1,2 @@
 def has_manage_schedule_permission(user, event):
-    if user.is_superuser:
-        return True
-    else:
-        return event.user_in_jury(user)
+    return user.is_superuser or event.user_in_jury(user)
