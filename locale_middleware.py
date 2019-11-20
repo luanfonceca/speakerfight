dd0cac152c13f849bbafb0a9cb01803716f49fe2
Luca Bezerra <lucabezerra@gmail.com>
2019-11-20 00:59:50 -0500

    Removing unnecessary comments.

--- locale_middleware.py
+++ locale_middleware.py
@@ -5,17 +5,9 @@ from django.utils import translation
 class LocaleMiddleware():
     def __init__(self, get_response):
         self.get_response = get_response
-        # One-time configuration and initialization.
 
     def __call__(self, request):
-        # Code to be executed for each request before
-        # the view (and later middleware) are called.
-
         response = self.get_response(request)
-
-        # Code to be executed for each request/response after
-        # the view is called.
-
         return response
 
     def process_request(self, request):
